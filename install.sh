81a07245888cd118628fe74f51fb459ac7edcca2
Jordan Rancie <jordan@thetradingbox.com>
2017-07-01 22:43:20 +0800

    Not yet

--- install.sh
+++ install.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/bash
 
-# TODO
- - Working on port 80
- - Finish full deploy script for server
-
 sudo apt-get install nodejs
 sudo apt-get install build-essential
 sudo npm install -g pm2
 
 # .bashrc - add 'export NODE_ENV="production"'
-# Copy over git deploy keys - rename to .ssh/id_rsa
+# Copy over git deploy keys
 # Copy over mailer-api/config.js
-# Copy over SSL Cert and chain
+# Copy over SSL Cert and key
 # git clone git@github.com:GDayDigitalNomads/mailer-api.git
+
 cd mailer-api
 sudo npm install -g
 sudo nom link
@@ -21,7 +18,7 @@ pm2 start mailer
 pm2 startup systemd
 # Follow the prompts
 
-sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 9100
+sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 9101
 
 
 
---
+++ secret/readme.txt
@@ -0,0 +1,1 @@
+This is where your SSL cert and private key go
